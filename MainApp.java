3c848e7ee41636d7eafc9b86fd2e88a1c12e75b0
Rebecca B <39128621+planocodegirl@users.noreply.github.com>
2019-07-25 20:56:46 -0500

    Upload of Java files

---
+++ MainApp.java
@@ -0,0 +1,15 @@
+package App;
+
+public class MainApp {
+	
+	 static aBeginning bg	 = new aBeginning();
+
+	public static void main(String[] args) {
+		//calling simple method
+		//to print out strings
+		bg.simpleMethod();
+		
+		
+	}
+
+}
