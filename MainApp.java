f9b9197d37aa5072fd7e386b63ec9e45c3ddcacc
Rebecca B <39128621+planocodegirl@users.noreply.github.com>
2019-07-25 21:06:00 -0500

    Changes to main method

--- MainApp.java
+++ MainApp.java
@@ -2,13 +2,18 @@ package App;
 
 public class MainApp {
 	
-	 static aBeginning bg	 = new aBeginning();
-
+	static aBeginning bg	 = new aBeginning();
+	static projectGutenberg pg = new projectGutenberg();
+		
+	
 	public static void main(String[] args) {
 		//calling simple method
 		//to print out strings
 		bg.simpleMethod();
+		System.out.println("-----------------------------------------------------------------------------");
 		
+		pg.gutenberg(args);
+		System.out.println("-----------------------------------------------------------------------------");
 		
 	}
 
