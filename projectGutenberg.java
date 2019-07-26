af026f04705ecdd603d5049ab2c6389e2a5b2312
Rebecca B <39128621+planocodegirl@users.noreply.github.com>
2019-07-25 21:04:06 -0500

    Another Method
    Adding additional method in another class to call from main app

---
+++ projectGutenberg.java
@@ -0,0 +1,13 @@
+package App;
+
+public class projectGutenberg {
+	public static void gutenberg(String[] args) {
+        System.out.println("Did you know that in 1440 (or thereabouts)");
+        System.out.println("Johannes Gutenberg invented the printing press?");
+        System.out.println("He started out as a goldsmith!");
+        System.out.println("His invention made rapid and prolific typed volumes");
+        System.out.println("Available for the first time ever...!");
+        System.out.println("We are like a modern Gutenberg!");
+        System.out.println("Printing vast amounts to the waiting console with ease!");
+    }
+}
