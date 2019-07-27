40fcdc506bdc6800e25432c71468a8895e8b7481
Rebecca B <39128621+planocodegirl@users.noreply.github.com>
2019-07-26 21:24:16 -0500

    All The Maths
    practicing using arithmetic operators and calling that class form the main app

--- MainApp.java
+++ MainApp.java
@@ -5,7 +5,7 @@ public class MainApp {
 	static aBeginning bg	 = new aBeginning();
 	static projectGutenberg pg = new projectGutenberg();
 	static CommentingCode cc = new CommentingCode();
-		
+	static AllTheMaths atm= new AllTheMaths();	
 	
 	public static void main(String[] args) {
 		//calling simple method
@@ -17,6 +17,12 @@ public class MainApp {
 		System.out.println("-----------------------------------------------------------------------------");
 		
 		cc.commenting();
+		System.out.println("-----------------------------------------------------------------------------");
+		
+		cc.commenting();
+		System.out.println("-----------------------------------------------------------------------------");
+		
+		atm.math();
 		System.out.println("-----------------------------------------------------------------------------");
 	}
 
